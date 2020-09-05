6f771f72818c449fa56f8e950afb6b9b26196682
Lucian nicolescu <lucian.nicolescu.cello@gmail.com>
2020-09-05 12:24:09 -0700

    added page transitions

--- app.js
+++ app.js
@@ -15,44 +15,68 @@ let activeBtn = aboutBtn
 
 function about() {
     activePage.classList.remove('activePage')
-    activePage.classList.add('hidden')
-    activeBtn.addEventListener('click', activeListener)
-    aboutPage.classList.remove('hidden')
-    aboutPage.classList.add('activePage')
-    activePage = aboutPage    
+    activePage.style.opacity = '0'
+    setTimeout(() => {
+        activePage.classList.add('hidden')
+        activePage = aboutPage    
+        aboutPage.classList.remove('hidden')
+        setTimeout(() => {
+            activePage.style.opacity = '1'
+            activeBtn.addEventListener('click', activeListener)
+            aboutPage.classList.add('activePage')}, 100)
+    }, 300)
 }
 
 function technologies() {
     activePage.classList.remove('activePage')
-    activePage.classList.add('hidden')
-    activeBtn.addEventListener('click', activeListener)
-    techPage.classList.remove('hidden')
-    techPage.classList.add('activePage')
-    activePage = techPage
+    activePage.style.opacity = '0'
+    setTimeout(() => {
+        activePage.classList.add('hidden')
+        activePage = techPage
+        activePage.classList.remove('hidden')
+        setTimeout(() => {
+            activePage.style.opacity = '1'
+            activeBtn.addEventListener('click', activeListener)
+            techPage.classList.add('activePage')}, 100)
+    }, 300)
 }
 
 function projects() {
     activePage.classList.remove('activePage')
-    activePage.classList.add('hidden')
-    activeBtn.addEventListener('click', activeListener)
-    projectsPage.classList.remove('hidden')
-    projectsPage.classList.add('activePage')
-    activePage = projectsPage    
+    activePage.style.opacity = '0'
+    setTimeout(() => {
+        activePage.classList.add('hidden')
+        activePage = projectsPage
+        activePage.classList.remove('hidden')
+        setTimeout(() => {
+            activePage.style.opacity = '1'
+            activeBtn.addEventListener('click', activeListener)
+            techPage.classList.add('activePage')}, 100)
+    }, 300)  
 }
 
 function contact() {
     activePage.classList.remove('activePage')
-    activePage.classList.add('hidden')
-    activeBtn.addEventListener('click', activeListener)
-    contactPage.classList.remove('hidden')
-    contactPage.classList.add('activePage')
-    activePage = contactPage    
+    activePage.style.opacity = '0'
+    setTimeout(() => {
+        activePage.classList.add('hidden')
+        activePage = contactPage
+        activePage.classList.remove('hidden')
+        setTimeout(() => {
+            activePage.style.opacity = '1'
+            activeBtn.addEventListener('click', activeListener)
+            techPage.classList.add('activePage')}, 100)
+    }, 300)    
 }
 
 
 //-------------------EVENT LISTENERS
 
 let activeListener = about
+// aboutPage.addEventListener('click', () => activePage.style.opacity = '0');
+// techPage.addEventListener('click', () => techPage.style.opacity = '1');
+// projectsPage.addEventListener('click', () => projectsPage.style.opacity = '1');
+// contactPage.addEventListener('click', () => contactPage.style.opacity = '1');
 techBtn.addEventListener('click', technologies)
 projectsBtn.addEventListener('click', projects)
 contactBtn.addEventListener('click', contact)
